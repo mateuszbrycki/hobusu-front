57df6570c4936d66afeada4d0fe7d294d1434fb7
Mateisz Brycki <mateusz.brycki@outlook.com>
2017-01-13 18:51:01 +0100

    F-TSN-2 - adding new transaction (form)

--- module/transaction/transaction.js
+++ module/transaction/transaction.js
@@ -1,10 +1,29 @@
-hobusu.controller('TransactionController', function($scope, $http, $cookies) {
-
-    $http.get(API_ADDRESS + "/transaction")
-        .then(function(data, status, headers, config) {
-            $scope.transactions = data.data;
-        });
+hobusu.controller('TransactionController', function ($scope, $http, transactionCategoryService) {
 
     $scope.orderByField = 'id';
     $scope.reverseSort = false;
+    $scope.format = 'yyyy-MM-dd';
+
+    transactionCategoryService.getList().then(function (data) {
+        $scope.categories = data;
+    });
+
+    $scope.$on('transactionListUpdate', function () {
+        $scope.get();
+    });
+
+    $scope.get = function () {
+        $http.get(API_ADDRESS + "/transaction")
+            .then(function (data, status, headers, config) {
+                $scope.transactions = data.data;
+            });
+    };
+
+    $scope.add = function (transaction) {
+        $http.post(API_ADDRESS + "/transaction", transaction)
+            .then(function (response) {
+                $scope.$broadcast('transactionListUpdate');
+            });
+    };
+
 });
